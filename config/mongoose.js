dcb52fc567ed6f830149bd445a2f2b5acf1fb86d
Rajeev <ronaldocrcr5@gmail.com>
2023-09-21 19:16:55 +0530

    initial commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose');
 // storing the db on mongo atlas
 const DB = "mongodb+srv://rajeevkrs:rajeevkr.450@cluster0.xigfak7.mongodb.net/?retryWrites=true&w=majority";
 
+mongoose.connect(DB).then(()=>{
+    console.log('Connection successful!');
+}).catch((err) => console.log("no connection " + err));
+
 // connected to the db
-// const DB = mongoose.connect('mongodb://127.0.0.1/todolist_db');
+// mongoose.connect('mongodb://127.0.0.1/todolist_db');
 
 // acquired connection to db
 const db=mongoose.connection;
